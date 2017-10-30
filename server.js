e2f41e2ec07dfe61adbb77be05cffd059dd1e13d
Howard Shaw <chip.shaw4@gmail.com>
2017-10-29 19:29:56 -0600

    server file

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+   console.log('serving yo');
+});
