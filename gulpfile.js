cdd3160c06599daa9e6c0f62b482dbd31cb5348e
Howard Shaw <chip.shaw4@gmail.com>
2017-10-29 19:30:26 -0600

    gulp setup

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+const gulp = require('gulp');
+const browserify = require('gulp-browserify');
+const clean = require('gulp-clean');
+
+const spawn = require('child_process').spawn;
+let node;
+
+
+gulp.task('server', function() {
+   if (node) {
+      node.kill();
+   }
+   node = spawn('node', ['server.js'], {stdio: 'inherit'});
+   node.on('close', function (code) {
+      if (code === 8) {
+         gulp.log('Error detected, waiting for changes...');
+      }
+   });
+})
+
+gulp.task('clean', function() {
+   return gulp
+   .src(['public/game-files/'])
+   .pipe(clean());
+});
+
+gulp.task('bundle', ['clean'], function() {
+   gulp
+   .src(['game-files/levels/level-*/level-*.js'])
+   .pipe(browserify({
+      insertGlobals: true,
+      debug: true
+   }))
+   .pipe(gulp.dest('public/game-files/'));
+});
+
+
+gulp.task('default', ['server'], function() {
+   gulp.watch([
+      '**/*.js', 
+      '!/public/**/*', 
+      '!node_modules/**/*', 
+      '!gulpfile.js'
+   ], [
+      'bundle',
+      'server'
+   ]);
+});
+
