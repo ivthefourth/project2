1c779ee36e9a8f82f58155cd0bef7d18da4c118c
Howard Shaw <chip.shaw4@gmail.com>
2017-10-31 18:28:09 -0600

    basic level routing

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const handlebars = require("express-handlebars");
 
 const routes = require('./routes/routes');
 
@@ -9,6 +10,9 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.engine("handlebars", handlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 app.use(express.static("public"));
 app.use(routes);
