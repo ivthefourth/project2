1248bb905661f4c07758bec47cb1b4d9d9f8db01
unknown <2krazykats@gmail.com>
2017-11-03 11:27:40 -0600

    set MySQL password to null

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ if (process.env.JAWSDB_URL) {
       port: 3306,
       host: "localhost",
       user: "root",
-      password: '0jsw693Ml',
+      password: null
       database: "game_db"
     });
   };
