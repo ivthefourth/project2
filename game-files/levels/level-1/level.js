dae1790e681d3e148cd613c2d8b586ac57d1516f
Howard Shaw <chip.shaw4@gmail.com>
2017-11-04 10:29:27 -0600

    hard level

--- game-files/levels/level-1/level.js
+++ game-files/levels/level-1/level.js
@@ -2,7 +2,7 @@ function succumbToDeath(){
    console.log('DIE');
 }
 
-var game = new Phaser.Game(800, 800, Phaser.AUTO, '', { 
+game = new Phaser.Game(1280, 720, Phaser.AUTO, '', { 
    preload: preload, 
    create: create, 
    update: update
@@ -15,7 +15,7 @@ function preload() {
 
    game.load.image('player', 'game-files/images/snitch-avatar.png');
 
-   game.load.tilemap('test', 'game-files/levels/level-1/template.json', null, Phaser.Tilemap.TILED_JSON);
+   game.load.tilemap('test', 'game-files/levels/level-1/test_level.json', null, Phaser.Tilemap.TILED_JSON);
    game.load.image('tiles', 'game-files/images/scifi.png');
 }
 
@@ -37,7 +37,9 @@ function create() {
 
     //  This resizes the game world to match the layer dimensions
     layer.resizeWorld();
-   player = game.add.sprite(100, 200, 'player');
+   player = game.add.sprite(100, 616, 'player');
+
+   camerafollow = game.add.sprite(game.camera.width/2.5,200);
 
     map.setCollisionBetween(1, 20);
     map.setCollisionBetween(25, 36);
@@ -49,6 +51,7 @@ function create() {
    ], succumbToDeath);
 
    game.physics.enable(player);
+   game.physics.enable(camerafollow);
 
    player.body.collideWorldBounds = true;
    player.body.gravity.y = 1000;
@@ -62,7 +65,7 @@ function create() {
    )
 
 
-    game.camera.follow(player, Phaser.Camera.FOLLOW_LOCKON, 0.1, 0.1);
+    game.camera.follow(camerafollow, Phaser.Camera.FOLLOW_LOCKON, 0.1, 0.1);
 }
 
 function update() {
@@ -70,18 +73,39 @@ function update() {
    game.physics.arcade.TILE_BIAS = 40;
    game.physics.arcade.collide(player, layer);
 
-   if(Math.abs(player.body.velocity.x) !== 0){
-      player.body.velocity.x -= Math.sign(player.body.velocity.x)*25
+   if(player.x + player.width * 0.75 < game.camera.x){
+    succumbToDeath();
    }
 
-   if (cursors.left.isDown && player.body.velocity.x > -250)
-   {
-      player.body.velocity.x += -50;
-   }
-   else if (cursors.right.isDown && player.body.velocity.x < 250)
+
+  if (player.body.velocity.x < 500)
    {
       player.body.velocity.x += 50;
    }
+  if (camerafollow.body.velocity.x < 500)
+   {
+      camerafollow.body.velocity.x += 50;
+   }
+   //camerafollow.y = player.y
+   camerafollow.y = game.camera.height/2;
+   if(camerafollow.x > game.world.width - (game.camera.width/2)){
+    camerafollow.body.velocity.x = 0;
+   }
+
+
+   // if(Math.abs(player.body.velocity.x) !== 0){
+   //    player.body.velocity.x -= Math.sign(player.body.velocity.x)*25
+   // }
+
+   // if (cursors.left.isDown && player.body.velocity.x > -500)
+   // {
+   //    player.body.velocity.x += -50;
+   // }
+   // else if (cursors.right.isDown && player.body.velocity.x < 500)
+   // {
+   //    player.body.velocity.x += 50;
+   // }
+
    if(jumpButton.isDown && Math.sign(player.body.velocity.y) != Math.sign(player.body.gravity.y)){
       player.body.velocity.y += Math.sign(player.body.gravity.y)*25; 
    }
